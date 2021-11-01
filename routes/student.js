121841cb66b27eb158b8dc218b9501a4ac830244
kar-thee <ourworldourpeople@gmail.com>
2021-11-01 21:15:44 +0530

    updated

--- routes/student.js
+++ routes/student.js
@@ -9,13 +9,18 @@ router.get("/", (req, res) => {
 
 router.post("/", async (req, res) => {
   console.log("Student create route");
-  const data = await student.create({
-    name: req.body.name,
-    email: req.body.email,
-    course: req.body.course,
-    mentorAssigned: req.body.mentorAssigned,
-  });
-  res.send(data);
+  try {
+    const data = await student.create({
+      name: req.body.name,
+      email: req.body.email,
+      course: req.body.course,
+      mentorAssigned: req.body.mentorAssigned,
+    });
+    res.send(data);
+  } catch (e) {
+    console.log("error");
+    res.status(400).send("Error in student POST");
+  }
 });
 
 router.put("/", (req, res) => {
